b0d445779eaee0f799ff07580becdc3f7e604644
piyaladamas2 <piyal.adamas2@gmail.com>
2025-06-25 11:37:16 +0530

    GlobalExceptionHandler updated

--- studentmanagement/src/main/java/com/piyal/studentmanagement/ExceptionHandler/GlobalExceptionHandler.java
+++ studentmanagement/src/main/java/com/piyal/studentmanagement/ExceptionHandler/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.piyal.studentmanagement.ExceptionHandler;
 import java.util.HashMap;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,6 +12,7 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
   @ExceptionHandler(ResourceNotFoundException.class)
   public ResponseEntity<Map<String, String>> handleResourceNotFound(ResourceNotFoundException ex) {
     Map<String, String> error = new HashMap<>();
@@ -22,6 +25,20 @@ public class GlobalExceptionHandler {
     Map<String, String> errors = new HashMap<>();
     ex.getBindingResult().getFieldErrors().forEach(err -> errors.put(err.getField(), err.getDefaultMessage()));
     return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(BadCredentialsException.class)
+  public ResponseEntity<Map<String, String>> handleBadCredentials(BadCredentialsException ex) {
+    Map<String, String> error = new HashMap<>();
+    error.put("error", "Invalid username or password");
+    return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+  }
+
+  @ExceptionHandler(UsernameNotFoundException.class)
+  public ResponseEntity<Map<String, String>> handleUsernameNotFound(UsernameNotFoundException ex) {
+    Map<String, String> error = new HashMap<>();
+    error.put("error", "User not found");
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler(Exception.class)
